06004344be3036b4d253d0b5c40d045c464a13c8
Shekhireva <Shehireva.87@yandex.ru>
2024-06-03 00:55:35 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,28 @@
+#1
+a= 'Cat'
+print(len(a))
+#2
+first= 25
+second= 15
+summa= (first + second)
+diff= first - second
+print(summa)
+print(diff)
+#3
+first= 2
+second= 5
+third= 6
+mean= (first + second + third) / (3)
+print(mean)
+#4
+first= 'Вторник'
+second= 'Понедельник, '
+print(second + first)
+#5
+a=2
+b=3
+c=6
+f=(a*b)+(a*c)
+d=(f**3)
+n=(d/2)
+print(n)
